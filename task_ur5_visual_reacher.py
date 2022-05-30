03cb3f261e582fad8d04e0d9bdc0e43df3a5859e
YufengYuan <yuanyufeng@outlook.com>
2022-05-30 11:44:03 -0600

    test

--- task_ur5_visual_reacher.py
+++ task_ur5_visual_reacher.py
@@ -80,7 +80,7 @@ def parse_args():
     parser.add_argument('--port', default=9876, type=int)
     parser.add_argument('--mode', default='ro', type=str, help="Modes in ['r', 'o', 'ro', 'e'] ")
     # misc
-    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--seed', default=9, type=int)
     parser.add_argument('--work_dir', default='.', type=str)
     parser.add_argument('--save_tb', default=False, action='store_true')
     parser.add_argument('--save_model', default=True, action='store_true')
