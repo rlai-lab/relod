12345885cdb128cb29f1ec584aede757afe46014
Gautham Vasan <gauthamv.529@gmail.com>
2023-07-07 15:47:33 -0600

    Save model

--- run_create2.sh
+++ run_create2.sh
@@ -1,2 +1,2 @@
 sudo chmod 777 /dev/ttyUSB0
-nohup python3 -u task_create2_visual_reacher.py 1> train.out 2> train.err &
+nohup python3 -u task_create2_visual_reacher.py --save_model --seed 1 --camera_id 0 --remote_ip "192.168.1.2" 1> train.out 2> train.err &
